366b4ef24c8598155b8730e69da3b231b6e31e18
kometanimizuki <rpslym1026@gmail.com>
2019-07-07 17:35:42 +0900

    change table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :members
+  has_many :group_user
   has_many :messages
-  has_many :groups, through: :members
+  has_many :groups, through: :group_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   get  'group/index'
-
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:index, :edit, :update] 
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]do
+    resources :messages, only: [:index, :create]
   end
 end
