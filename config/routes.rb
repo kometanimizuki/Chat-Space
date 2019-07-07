a592fedba879248eae79873c25f13472806ba754
kometanimizuki <rpslym1026@gmail.com>
2019-07-07 15:56:27 +0900

    Summary

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   get  'group/index'
+
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create]do
   end
 end
