491daad19ae94c36bcf25f5f3f3d2ea988aecb76
kometanimizuki <rpslym1026@gmail.com>
2019-07-27 18:36:51 +0900

    Edit serch

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -13,7 +13,7 @@ $(document).on('turbolinks:load', function() {
   }
 
     function appendMember(id, name) {
-      var html = `<div class="chat-group-user clearfix chat-group-form__field>
+      var html = `<div class="chat-group-user clearfix chat-group-form__field">
                     <input name="group[user_ids][]" type="hidden" value=${id}>
                     <p class="chat-group-user__name">${name}</p>
                     <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove">削除</div>
@@ -24,13 +24,9 @@ $(document).on('turbolinks:load', function() {
   $('#user-search-result').on('click', '.user-search-add', function() {
     var id = $(this).attr('data-user-id');
     var name = $(this).attr('data-user-name');
-    console.log(id)
-    console.log(name)
     var user = appendMember(id, name)
-    console.log(user)
     $('#chat-group-users').append(user)
     $(this).parent().remove();
-    console.log(this)
   })
 
   $(document).on('click', '.user-search-remove', function() {
