08546914e8c1df2ff4be6b84e40263b0d83567b7
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-04 19:40:06 +0900

    Change Gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chatspace1026'
-  config.asset_host = 'https://s3-ap-northeast-1' #(※例 ap-northeast-1).amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace1026' #(※例 ap-northeast-1).amazonaws.com/ここにバケット名を入れます'
 end
