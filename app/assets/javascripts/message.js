384896568aff78f72e8a84d5f768a44c71509627
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-08-08 21:34:23 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -68,7 +68,7 @@ $(document).on('turbolinks:load', function(){
           insertHTML = buildHTML(message);
           $('.messages').append(insertHTML);
         })
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        $('.messages').animate({scrollTop: $('.messages')[60].scrollHeight}, 'fast');
       })
       .fail(function () {
         alert('自動更新に失敗しました');
